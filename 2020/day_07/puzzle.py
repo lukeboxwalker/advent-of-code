7ace25a26a166d0a4346b39016dc6c2371150e68
lukeboxwalker <lukas.walkenhorst@t-online.de>
2020-12-07 18:38:22 +0100

    Solve day 7

--- 2020/day_07/puzzle.py
+++ 2020/day_07/puzzle.py
@@ -1,20 +1,50 @@
-def read_input(filename: str) -> list:
+import re
+
+
+def read_input(filename: str) -> map:
+    regex = re.compile(r" bags?")
+    result = {}
     with open(filename, "r") as f:
-        return f.read().splitlines()
+        for bag in f.read().splitlines():
+            key, value_data = bag[:-1].split(" bags contain ")
+            values = {}
+            if value_data != "no other bags":
+                for val in [re.sub(regex, "", val) for val in value_data.split(", ")]:
+                    values[val[2:]] = int(val[0])
+            result[key] = values
+    return result
 
 
-def part_1(values: list) -> int:
-    return -1
+def find_bags(bags: map, search_bag: str, result: set) -> set:
+    for bag in bags:
+        if bag != search_bag and search_bag in bags.get(bag):
+            result.add(bag)
+            find_bags(bags, bag, result)
+    return result
 
 
-def part_2(values: list) -> int:
-    return -1
+def part_1(bags: map) -> int:
+    return len(find_bags(bags, "shiny gold", set()))
+
+
+def count_bags(bags: map, count_bag: str) -> int:
+    count = 1
+    for bag in bags.get(count_bag):
+        count += bags.get(count_bag).get(bag) * count_bags(bags, bag)
+    return count
+
+
+def part_2(bags: map) -> int:
+    return count_bags(bags, "shiny gold") - 1
 
 
 if __name__ == '__main__':
     test_input = read_input("test.txt")
-    assert part_1(test_input) == -1
-    assert part_2(test_input) == -1
+
+    assert part_1(test_input) == 4
+    assert part_2(test_input) == 32
+
+    assert part_2(read_input("test_nested_bags.txt")) == 126
 
     my_input = read_input("input.txt")
     print("Puzzle 1:", part_1(my_input))
