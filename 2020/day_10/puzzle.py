370b07c82d3b611349d2688d13ea2d5d8d8c7abd
lukeboxwalker <lukas.walkenhorst@t-online.de>
2020-12-15 10:52:56 +0100

    Improve solution for day 10

--- 2020/day_10/puzzle.py
+++ 2020/day_10/puzzle.py
@@ -9,21 +9,6 @@ def part_1(values: list) -> int:
     return result.count(1) * result.count(3)
 
 
-# def sum_possible_ways(value_set: set, current: int, possible_ways: dict) -> int:
-#     if current in possible_ways:
-#         return possible_ways[current]
-#     possible_ways_for_current = 0
-#     for next_current in [current + 1, current + 2, current + 3]:
-#         if next_current in value_set:
-#             possible_ways_for_current += sum_possible_ways(value_set, next_current, possible_ways)
-#     possible_ways[current] = possible_ways_for_current
-#     return possible_ways_for_current
-#
-#
-# def part_2(values: list) -> int:
-#     return sum_possible_ways(set(values), 0, {values[-1]: 1})
-
-
 def part_2(values: list) -> int:
     poss_ways = [1] + [0] * values[-1]
     for i in values[1:]:
