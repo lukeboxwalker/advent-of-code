e758dbc910a8445aa90ec8f078b1c67c0c82c30b
lukeboxwalker <lukas.walkenhorst@t-online.de>
2021-12-01 16:10:25 +0100

    Solve day 1

---
+++ year_2021/day_01/puzzle.py
@@ -0,0 +1,21 @@
+def read_input(filename: str) -> list:
+    with open(filename, "r") as f:
+        return list(map(int, f.read().splitlines()))
+
+
+def part_1(values: list) -> int:
+    return sum(map(lambda x: x[0] < x[1], zip(values[:-1], values[1:])))
+
+
+def part_2(values: list) -> int:
+    return part_1(list(map(sum, zip(values[:-2], values[1:-1], values[2:]))))
+
+
+if __name__ == '__main__':
+    test_input = read_input("test_input.txt")
+    assert part_1(test_input) == 7
+    assert part_2(test_input) == 5
+
+    my_input = read_input("input.txt")
+    print(f"Part 1: {part_1(my_input)}")
+    print(f"Part 2: {part_2(my_input)}")
