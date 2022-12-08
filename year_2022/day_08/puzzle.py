d71fcfa77a04911af13ee43f56048d5b02b5b16d
lukeboxwalker <lukas.walkenhorst@t-online.de>
2022-12-08 17:54:14 +0100

    Simplify day 8

--- year_2022/day_08/puzzle.py
+++ year_2022/day_08/puzzle.py
@@ -8,20 +8,26 @@ def read_input(filename: str) -> ndarray:
     with open(filename, "r") as f:
         return np.array([list(map(int, row)) for row in f.read().splitlines()])
 
+
+def directions(values, i, j):
+    return [list(reversed(values[i][:j])), values[i][j + 1:],  # left, right
+            list(reversed(values[:, j][:i])), values[:, j][i + 1:]]  # top, bottom
+
+
 def part_1(values: ndarray) -> int:
-    count = 0
-    for i, j in np.ndindex(values.shape):
-        sides = Stream([values[i][:j], values[i][j + 1:], values[:, j][:i], values[:, j][i + 1:]]) \
-            .map(lambda x: x if x.size > 0 else np.array([-1])).list()
-        count += int(Stream(sides).map(lambda x: max(x) < values[i, j]).reduce(lambda a, b: a or b))
-    return count
+    return Stream(np.ndindex(values.shape)).map(
+        lambda x: Stream(directions(values, x[0], x[1]))
+            .map(lambda y: max(y) if len(y) > 0 else -1)
+            .map(lambda y: y < values[x[0], x[1]])
+            .reduce(lambda a, b: a or b)
+    ).map(int).sum()
 
 
 def part_2(values: ndarray) -> int:
     count = []
     for i, j in np.ndindex(values.shape):
         scenic_score = [0, 0, 0, 0]
-        sides = [reversed(values[i][:j]), values[i][j + 1:], reversed(values[:, j][:i]), values[:, j][i + 1:]]
+        sides = directions(values, i, j)
         for k in range(4):
             for m in sides[k]:
                 scenic_score[k] += 1
