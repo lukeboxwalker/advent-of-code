453b7b4014bcde406dfa79ab0200f976549122a7
lukeboxwalker <lukas.walkenhorst@t-online.de>
2023-12-01 17:24:05 +0100

    Update solution day 16

--- year_2022/day_16/puzzle.py
+++ year_2022/day_16/puzzle.py
@@ -11,11 +11,12 @@ class Valve:
     def __init__(self, name, rate, neighbors):
         self.name = name
         self.rate = rate
+        self.idx = 0
         self.neighbors = neighbors
         self.connections = {name: 0}
 
     def __str__(self) -> str:
-        return f"{self.name}, {self.rate}, {self.connections}"
+        return f"{self.name}, {self.rate}, {self.idx}, {self.connections}"
 
 
 def read_input(filename: str) -> dict:
@@ -34,6 +35,9 @@ def read_input(filename: str) -> dict:
                 valves[name].connections.pop(connection)
         if valves[name].rate == 0 and name != "AA":
             valves.pop(name)
+    keys = list(valves.keys())
+    for i in range(len(keys)):
+        valves[keys[i]].idx = 0x1 << i
     return valves
 
 def min_distance_valve(connections, valves, neighbors, distance):
@@ -43,47 +47,35 @@ def min_distance_valve(connections, valves, neighbors, distance):
             min_distance_valve(connections, valves, valves[neighbor].neighbors, distance + 1)
 
 def part_1(valves: dict) -> int:
-    return depth_search(valves, "AA", set(), 30, dict())
+    return depth_search(valves, "AA", 0, 30, dict())
 
 def part_2(valves: dict) -> int:
-    combination_cache = dict()
     depth_cache = dict()
     max_release = 0
-    for subset in itertools.chain.from_iterable(itertools.combinations(valves.keys(), r) for r in range(len(valves) + 1)):
-        valves_elephant = set(subset)
-        valves_me = set(valves.keys()) - valves_elephant
-        combination_key = (tuple(sorted(list(valves_elephant))), tuple(sorted(list(valves_me))))
-        if len(valves_me) > len(valves_elephant):
-            combination_key = (tuple(sorted(list(valves_me))), tuple(sorted(list(valves_elephant))))
-        if combination_key in combination_cache:
-            if combination_cache[combination_key] > max_release:
-                max_release = combination_cache[combination_key]
-            continue
-        combination_release = depth_search(valves, "AA", valves_elephant, 26, depth_cache) + depth_search(valves, "AA", valves_me, 26, depth_cache)
+    size = len(valves)
+    for closed_valves in range(2**size // 2):
+        closed_valves_elephant = closed_valves ^ (2 ** size - 1)
+        combination_release = depth_search(valves, "AA", closed_valves, 26, depth_cache) + depth_search(valves, "AA", closed_valves_elephant, 26, depth_cache)
         if combination_release > max_release:
-            combination_cache[combination_key] = combination_release
             max_release = combination_release
             continue
     return max_release
 
-
-def depth_search(valves: dict, current_valve: str, closed_valves: set, minute: int, cache: dict):
+def depth_search(valves: dict, current_valve: str, closed_valves: int, minute: int, cache: dict):
     if minute <= 0:
         return 0
-    cache_key = (current_valve, tuple(sorted(list(closed_valves))), minute)
+    cache_key = (current_valve, closed_valves, minute)
     if cache_key in cache:
         return cache[cache_key]
     release_over_time = valves[current_valve].rate * minute
     release_over_time_from_neighbor = 0
     for neighbor_valve, distance in valves[current_valve].connections.items():
-        if neighbor_valve in closed_valves:
+        if valves[neighbor_valve].idx & closed_valves != 0:
             continue
         if distance > minute:
             continue
-        closed_valves_at_neighbor = set(closed_valves)
-        closed_valves_at_neighbor.add(current_valve)
         minute_at_neighbor = minute - distance - 1
-        release_over_time_from_current_neighbor = depth_search(valves, neighbor_valve, closed_valves_at_neighbor, minute_at_neighbor, cache)
+        release_over_time_from_current_neighbor = depth_search(valves, neighbor_valve, closed_valves | valves[current_valve].idx, minute_at_neighbor, cache)
         if release_over_time_from_current_neighbor > release_over_time_from_neighbor:
             release_over_time_from_neighbor = release_over_time_from_current_neighbor
     cache[cache_key] = release_over_time + release_over_time_from_neighbor
