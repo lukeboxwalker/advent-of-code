aa594e6052ad3897cbbadf65e5fd6f8f400beb10
lukas <lukas.walkenhorst@t-online.de>
2025-12-06 21:21:33 +0100

    Shortn 2025 day 6

--- year_2025/day_06/puzzle.py
+++ year_2025/day_06/puzzle.py
@@ -2,41 +2,28 @@ import re
 
 from aoc.api import *
 
+
 def read_input(filename: str) -> list:
     return FileStream(filename).list()
 
 
 def part_1(values: list) -> int:
-    numbers = Stream(values[:len(values) - 1]).map(lambda x: Stream(re.findall("(\\d+) *", x)).map(int).list()).list()
-    operations = [x for x in list(values[-1]) if x != " "]
-    result = 0
-    for col_index in range(len(numbers[0])):
-        col = [row[col_index] for row in numbers]
-        if operations[col_index] == "+":
-            result += sum(col)
-        if operations[col_index] == "*":
-            result += prod(col)
-    return result
+    numbers = list(map(lambda x: list(map(int, re.findall("(\\d+) *", x))), values[:len(values) - 1]))
+    operations = [sum if x == "+" else prod for x in list(values[-1]) if x != " "]
+    return sum(operations[col_index]([row[col_index] for row in numbers]) for col_index in range(len(numbers[0])))
 
 
 def part_2(values: list) -> int:
-    operations = [(x, []) for x in list(values[-1]) if x != " "]
-    grid = values[:len(values) - 1]
-    max_len = max(map(len, grid))
+    operations = [(sum if x == "+" else prod, []) for x in list(values[-1]) if x != " "]
+    max_len = max(map(len, values[:len(values) - 1]))
     operation_index = 0
-    for col_index in range(max_len + 1):
-        col = ''.join(["" if len(row)-1 < col_index else row[col_index] for row in grid]).replace(" ","")
+    for i in range(max_len + 1):
+        col = ''.join(["" if len(r) - 1 < i else r[i] for r in values[:len(values) - 1]]).replace(" ", "")
         if col.isdigit():
             operations[operation_index][1].append(int(col))
         else:
             operation_index += 1
-    result = 0
-    for operation in operations:
-        if operation[0] == "+":
-            result += sum(operation[1])
-        if operation[0] == "*":
-            result += prod(operation[1])
-    return result
+    return sum(map(lambda x: x[0](x[1]), operations))
 
 
 if __name__ == '__main__':
